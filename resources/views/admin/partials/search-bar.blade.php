b8d7c4c765e26957805697b74ed35fe43a8680af
Ashir Afridi <ashirchannel765@gmail.com>
2023-12-05 08:48:57 +0500

    download btn

--- resources/views/admin/partials/search-bar.blade.php
+++ resources/views/admin/partials/search-bar.blade.php
@@ -1,6 +1,6 @@
 <div class="card mb-4 p-3">
     <form class="row" method="GET">
-        <div class="col-md-4">
+        <div class="col-md-6">
 
         </div>
         <div class="col-md-4">
@@ -13,12 +13,6 @@
             <div class="form-group">
                 <label for="" class="form-label">&nbsp;</label>
                 <input type="submit" value="Search" name="submit" class="form-control btn btn-primary">
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="form-group">
-                <label for="" class="form-label">&nbsp;</label>
-                <input type="submit" value="Download CSV" name="download" class="form-control btn btn-secondary">
             </div>
         </div>
     </form>
