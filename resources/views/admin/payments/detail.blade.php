4cb8dca9abe0bdc91401b4ea6254406481e4b60b
Ashir Afridi <ashirchannel765@gmail.com>
2024-01-05 01:17:50 +0500

    21-12-23

--- resources/views/admin/payments/detail.blade.php
+++ resources/views/admin/payments/detail.blade.php
@@ -9,40 +9,45 @@
         <div class="card">
             <div class="card-header d-flex justify-content-between align-items-center">
                 <h5>{{ $title }}<h5>
-                <a href="{{route('admin.payments.edit',$payment->id)}}" class="btn btn-primary">Update Status</a>
+                        <a href="{{ route('admin.payments.edit', $payment->id) }}" class="btn btn-primary">Update Status</a>
             </div>
 
             <div class="card-body">
                 <form method="POST">
-                    
+
                     <div class="row mb-4">
                         <div class="col-md-4">
                             <label class="form-label">User</label>
-                            <input type="text" class="form-control" value="{{$payment->user()->fullname()}}" name="amount" required placeholder="999" />
-                           
+                            <input type="text" class="form-control"
+                                value="{{ $item->userId != null ? $item->user()->fullname() : $item->vendor()->name . 'Vendor' }}"
+                                name="amount" required placeholder="999" />
+
                             @error('userId')
                                 <div class="form-text text-danger">
-                                    {{$message}}
+                                    {{ $message }}
                                 </div>
                             @enderror
                         </div>
 
                         <div class="col-md-4">
                             <label class="form-label">Amount</label>
-                            <input type="text" class="form-control" value="{{$payment->amount}} {{settings('currency_symbol')}}" name="amount" required placeholder="999" />
+                            <input type="text" class="form-control"
+                                value="{{ $payment->amount }} {{ settings('currency_symbol') }}" name="amount" required
+                                placeholder="999" />
                             @error('amount')
                                 <div class="form-text text-danger">
-                                    {{$message}}
+                                    {{ $message }}
                                 </div>
                             @enderror
                         </div>
 
                         <div class="col-md-4">
                             <label class="form-label">Payment Date</label>
-                            <input class="form-control" type="datetime-local" required name="date" value="{{$payment->paymentDate}}" />
+                            <input class="form-control" type="datetime-local" required name="date"
+                                value="{{ $payment->paymentDate }}" />
                             @error('date')
                                 <div class="form-text text-danger">
-                                    {{$message}}
+                                    {{ $message }}
                                 </div>
                             @enderror
                         </div>
@@ -50,31 +55,33 @@
                     <div class="row mb-4">
                         <div class="col-md-6">
                             <label class="form-label">Reference</label>
-                            <input type="text" class="form-control" value="{{$payment->reference}}" name="amount" required placeholder="999" />
-                           
+                            <input type="text" class="form-control" value="{{ $payment->reference }}" name="amount"
+                                required placeholder="999" />
+
                             @error('userId')
                                 <div class="form-text text-danger">
-                                    {{$message}}
+                                    {{ $message }}
                                 </div>
                             @enderror
                         </div>
 
                         <div class="col-md-6">
                             <label class="form-label">Status</label>
-                            <input type="text" class="form-control" value="{{$payment->status}}" name="amount" required placeholder="999" />
+                            <input type="text" class="form-control" value="{{ $payment->status }}" name="amount"
+                                required placeholder="999" />
                             @error('amount')
                                 <div class="form-text text-danger">
-                                    {{$message}}
+                                    {{ $message }}
                                 </div>
                             @enderror
                         </div>
 
                     </div>
-                    
+
                     <div class="row mb-4">
                         <div class="col-md-6">
                             <label class="form-label">For transaction</label>
-                            <textarea type="text" name="description" class="form-control" placeholder="Description..." rows="3">{{$payment->transaction()->description}}</textarea>
+                            <textarea type="text" name="description" class="form-control" placeholder="Description..." rows="3">{{ $payment->transaction()->description }}</textarea>
                             @error('description')
                                 <div class="form-text text-danger">
                                     {{ $message }}
@@ -83,8 +90,8 @@
                         </div>
                         <div class="col-md-6">
                             <label class="form-label">Screenshot</label>
-                            <img class="form-control" src="{{ $payment->screenshot}}" />
-                            
+                            <img class="form-control" src="{{ $payment->screenshot }}" />
+
                         </div>
                     </div>
                 </form>
