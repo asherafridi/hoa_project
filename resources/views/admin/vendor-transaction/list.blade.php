2724eded34d0b6c8d2e8a77035f9570b6c301ee8
Ashir Afridi <ashirchannel765@gmail.com>
2024-01-05 11:22:36 +0500

    21-12-23

--- resources/views/admin/vendor-transaction/list.blade.php
+++ resources/views/admin/vendor-transaction/list.blade.php
@@ -10,7 +10,7 @@
         <div class="card">
             <div class="card-header d-flex justify-content-between align-items-center">
                 <h5>{{ $title }}<h5>
-                        <a href="{{ route('admin.vendor-transaction.create') }}" class="btn btn-primary">Add Vendor
+                        <a href="{{ route('admin.vendor-transaction.create') }}" class="btn btn-primary">Add
                             {{ $title }}</a>
             </div>
 
