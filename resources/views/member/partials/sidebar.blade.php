9a1f29e8fdeb5dafcca30024859ea0ed0ecc3cb9
Ashir Afridi <ashirchannel765@gmail.com>
2024-01-14 23:34:30 +0500

    1-9-24

--- resources/views/member/partials/sidebar.blade.php
+++ resources/views/member/partials/sidebar.blade.php
@@ -31,7 +31,7 @@
         <li class="menu-item @if (str_contains($title, 'Work Order') == 1) active open @endif">
 
             <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bx-group"></i>
+                <i class="menu-icon tf-icons bx bx-basket"></i>
                 <div data-i18n="Layouts">Work Order</div>
             </a>
 
@@ -82,7 +82,7 @@
         <li class="menu-item @if (str_contains($title, 'Payment') == 1) active open @endif">
 
             <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bx-dollar-circle"></i>
+                <i class="menu-icon tf-icons bx bx-money"></i>
                 <div data-i18n="Layouts">Payments</div>
             </a>
 
@@ -130,7 +130,7 @@
 
         <li class="menu-item @if (str_contains($title, 'Polls') == 1) active @endif">
             <a href="{{ route('polls.index') }}" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-bell"></i>
+                <i class="menu-icon tf-icons bx bx-poll"></i>
                 <div data-i18n="Boxicons">Polls</div>
             </a>
         </li>
