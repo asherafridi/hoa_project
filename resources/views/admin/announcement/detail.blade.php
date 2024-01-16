c26a12fd3d8a339ac0634cf87aec547e70b95e61
Ashir Afridi <ashirchannel765@gmail.com>
2024-01-16 14:51:55 +0500

    1-15-24

--- resources/views/admin/announcement/detail.blade.php
+++ resources/views/admin/announcement/detail.blade.php
@@ -1,4 +1,5 @@
 @extends('admin.layouts.main')
+
 @section('title', $title)
 
 @section('content')
@@ -8,51 +9,44 @@
 
         <div class="card">
             <div class="card-header d-flex justify-content-between align-items-center">
-                <h5>{{ $title }}<h5>
+                <h5>{{ $title }}</h5>
             </div>
 
             <div class="card-body">
-                <form method="POST">
-                    
-                    <div class="row mb-4">
-                        <div class="col-md-6">
-                            <label class="form-label">Title</label>
-                            <input type="text" value="{{$announcement->title}}" class="form-control" required name="title" placeholder="Arrangement"
-                                autofocus />
-                            @error('title')
-                                <div class="form-text text-danger">
-                                    {{ $message }}
-                                </div>
-                            @enderror
-                        </div>
-                        <div class="col-md-6">
-                            <label class="form-label">Date</label>
-                            <input type="datetime-local"  value="{{$announcement->date}}" class="form-control" required name="date" placeholder="Arrangement"
-                                autofocus />
-                            @error('date')
-                                <div class="form-text text-danger">
-                                    {{ $message }}
-                                </div>
-                            @enderror
-                        </div>
-
-
+                <div class="row mb-4">
+                    <div class="col-md-6">
+                        <strong>Title:</strong>
+                        <p>{{ $announcement->title }}</p>
+                        @error('title')
+                            <div class="form-text text-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
-                    <div class="row mb-4">
-                        <div class="col-md-12">
-                            <label class="form-label">Description</label>
-                            <textarea type="text" name="description" class="form-control" placeholder="Description..." rows="3"> {{$announcement->description}}</textarea>
-                            @error('description')
-                                <div class="form-text text-danger">
-                                    {{ $message }}
-                                </div>
-                            @enderror
-                        </div>
+                    <div class="col-md-6">
+                        <strong>Date:</strong>
+                        <p>{{ $announcement->date }}</p>
+                        @error('date')
+                            <div class="form-text text-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
-                </form>
+                </div>
+
+                <div class="row mb-4">
+                    <div class="col-md-12">
+                        <strong>Description:</strong>
+                        <p>{{ $announcement->description }}</p>
+                        @error('description')
+                            <div class="form-text text-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
             </div>
         </div>
-
-
     </div>
-@stop
+
+@endsection
