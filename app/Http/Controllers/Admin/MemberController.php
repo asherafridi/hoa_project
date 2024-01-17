42a07736d78bab6dd99e7b78523b53e8d9c1cc2d
Ashir Afridi <ashirchannel765@gmail.com>
2024-01-17 13:36:03 +0500

    1-15-24

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -100,8 +100,8 @@ class MemberController extends Controller
                 $additionStr .= $user->updated_at . ',';
                 $additionStr .= $user->userType != null ? $user->type()->name : 'Member Type Not Found';
                 $additionStr .= ',' . ($user->propertyId != null ? $user->property()->name : 'Property Not Found');
-                $additionStr .= ',' . ($user->propertyId != null ? $user->property()->phase() : '');
-                $additionStr .= ',' . ($user->propertyId != null ? $user->property()->block() : '');
+                $additionStr .= ',' . ($user->propertyId != null ? $user->phase() : '');
+                $additionStr .= ',' . ($user->propertyId != null ? $user->block() : '');
                 $csvContent .= $additionStr . "\n";
             }
 
